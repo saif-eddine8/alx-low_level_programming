9b4731adf74bd4e9e170d99eb7ca9e9bd9d220cc
Saif <Saif@gmail.com>
2023-06-20 17:48:26 -0700

    putchar file

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  main - Entry point
+ *  Return: 0 success
+ */
+int main(void)
+{
+	char c[] = "main";
+	int i;
+
+	for (i = 0; i < 9; i++)
+        {
+		_putchar(c[i]);
+	}
+        _putchar('\n');
+	return (0);
+}
